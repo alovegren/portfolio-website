810d770ad71b8bde99f1a95d9e397c56be2a8401
Missy <audrey.lovegren@utexas.edu>
2020-02-05 20:50:39 -0500

    new plz hire me viget page

--- js/js.js
+++ js/js.js
@@ -1,3 +1,17 @@
 var d = new Date();
 var n = d.getFullYear();
 document.getElementById("copyrightdate").innerHTML = n;
+
+let fitClass = $(".fit")
+
+var fitTextHeight = fitClass.css("height");
+var fitLineHeight = fitClass.css("line-height");
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+var missySigVertical = 10 + (512/154);
+console.log((((100 - missySigVertical - parseFloat(fitTextHeight))/2)vh));
+
+
+//  = (100 - missySigVertical - fitTextHeight)/2;
